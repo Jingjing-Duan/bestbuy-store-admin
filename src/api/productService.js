97902557ad8db54ccc04f8c9d2533be4dad5093e
Jingjing Duan <duan0029@algonquinlive.com>
2026-04-23 08:18:06 -0400

    fix: nignx setting

--- src/api/productService.js
+++ src/api/productService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001', // 如果你的真实地址不同，就改这里
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json'
   }
